6a50c240031106cea1610ab3fa71ca406fc046c4
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2025-10-03 11:13:56 -0300

    1

--- js/principal.js
+++ js/principal.js
@@ -9,6 +9,7 @@ var peso = tdPeso.textContent;
 console.log(paciente);
 console.log(peso);
 
+// proxima aula verificar esse 
 var tdAltura = document.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 console.log(altura);
