59bc3ef299a02a87517f5c4681bf3f1cf2830f62
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2025-10-07 09:47:02 -0300

    1

--- js/principal.js
+++ js/principal.js
@@ -6,7 +6,7 @@ titulo.textContent = ("Aparecida Nutricionista");
 var paciente = document.querySelector("#primeiro-paciente");
 var tdNome = document.querySelector(".info-nome");
 var nome = tdNome.textContent; 
-console.log(tdNome);
+console.log(nome);
 
 var tdPeso = document.querySelector(".info-peso");
 var peso = tdPeso.textContent;
