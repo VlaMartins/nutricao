80d7911495ee3bb89159f976115ca5632046d82f
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2025-10-10 09:47:50 -0300

    1

--- js/principal.js
+++ js/principal.js
@@ -20,14 +20,22 @@ var tdImc = paciente.querySelector(".info-imc");
 var imc = peso / (altura * altura); 
 tdImc.textContent = imc;
 console.log(imc);
+ 
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 if (peso < 0 || peso > 1000) {
     console.log("Peso inválido");
+    pesoEhValido = false;
 }
 
 if (altura < 0 || altura > 3.00) {
     console.log("Altura inválida!");
+    alturaEhValida = false;
 }
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
 
 
 
