1770e34dc448acd7fbf88d371abb252c9bd4742f
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2025-10-10 10:07:07 -0300

    1

--- js/principal.js
+++ js/principal.js
@@ -26,11 +26,13 @@ var alturaEhValida = true;
 if (peso < 0 || peso > 1000) {
     console.log("Peso inválido");
     pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
 }
 
 if (altura < 0 || altura > 3.00) {
     console.log("Altura inválida!");
     alturaEhValida = false;
+    tdImc.textContent = "Altura Inválida!";
 }
 
 if (alturaEhValida && pesoEhValido){
