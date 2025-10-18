b8b88a9e00dc41965ee14fc9211e112effe2876f
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2025-10-18 14:33:52 -0300

    2

--- js/principal.js
+++ js/principal.js
@@ -7,33 +7,30 @@ var pacientes = document.querySelectorAll(".paciente");
 
 for (var i = 0; i <pacientes.length; i++){
     var paciente = pacientes[i]; 
-    
+
     var tdNome = document.querySelector(".info-nome");
     var nome = tdNome.textContent; 
-    console.log(nome);
 
     var tdPeso = document.querySelector(".info-peso");
     var peso = tdPeso.textContent;
-    console.log(peso);
 
     var tdAltura = document.querySelector(".info-altura");
     var altura = tdAltura.textContent;
-    console.log(altura);
 
     var tdImc = paciente.querySelector(".info-imc");
-    console.log(imc);
+
     
     //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
     var pesoEhValido = true;
     var alturaEhValida = true;
 
-    if (peso < 0 || peso > 1000) {
+    if (peso <= 0 || peso >= 1000) {
         console.log("Peso inválido");
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido!";
     }
 
-    if (altura < 0 || altura > 3.00) {
+    if (altura <= 0 || altura >= 3.00) {
         console.log("Altura inválida!");
         alturaEhValida = false;
         tdImc.textContent = "Altura Inválida!";
@@ -41,7 +38,7 @@ for (var i = 0; i <pacientes.length; i++){
 
     if (alturaEhValida && pesoEhValido){
         var imc = peso / (altura * altura); 
-        tdImc.textContent = imc;
+        tdImc.textContent = imc.toFixed(2);
     }
 }
 
