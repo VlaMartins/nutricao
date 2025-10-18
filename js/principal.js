7ee74699d8372bb42943d4ec196c6896b8447004
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2025-10-18 15:07:48 -0300

    22

--- js/principal.js
+++ js/principal.js
@@ -3,77 +3,77 @@ console.log(titulo);
 console.log(titulo.textContent);
 titulo.textContent = ("Aparecida Nutricionista");
 
-// var pacientes = document.querySelectorAll(".paciente");
-
-// for (var i = 0; i < pacientes.length; i++){
-//     // onde ela ira começar ; ate´onde ir; somar de 1 em 1
- 
-//     var paciente = pacientes[i]; 
-
-//     var tdPeso = document.querySelector(".info-peso");
-//     var peso = tdPeso.textContent;
-
-//     var tdAltura = document.querySelector(".info-altura");
-//     var altura = tdAltura.textContent;
-
-//     var tdImc = paciente.querySelector(".info-imc");
-    
-//     var pesoEhValido = true;
-//     var alturaEhValida = true;
-
-//     if (peso <= 0 || peso >= 1000) {
-//         console.log("Peso inválido");
-//         pesoEhValido = false;
-//         tdImc.textContent = "Peso inválido!";
-//     }
-
-//     if (altura <= 0 || altura >= 3.00) {
-//         console.log("Altura inválida!");
-//         alturaEhValida = false;
-//         tdImc.textContent = "Altura Inválida!";
-//     }
-
-//     if (alturaEhValida && pesoEhValido){
-//         var imc = peso / (altura * altura); 
-//         tdImc.textContent = imc;
-//     }
-// }
-
-// 21/10
 var pacientes = document.querySelectorAll(".paciente");
 
-for (var i = 0; i < pacientes.length; i++) {
+for (var i = 0; i < pacientes.length; i++){
+    // onde ela ira começar ; ate´onde ir; somar de 1 em 1
+ 
+    var paciente = pacientes[i]; 
 
-    var paciente = pacientes[i];
-
-    var tdPeso = paciente.querySelector(".info-peso");
+    var tdPeso = document.querySelector(".info-peso");
     var peso = tdPeso.textContent;
 
-    var tdAltura = paciente.querySelector(".info-altura");
+    var tdAltura = document.querySelector(".info-altura");
     var altura = tdAltura.textContent;
 
     var tdImc = paciente.querySelector(".info-imc");
-
+    
     var pesoEhValido = true;
     var alturaEhValida = true;
 
     if (peso <= 0 || peso >= 1000) {
-        console.log("Peso inválido!");
+        console.log("Peso inválido");
         pesoEhValido = false;
-        tdImc.textContent = "Peso inválido";
+        tdImc.textContent = "Peso inválido!";
     }
 
     if (altura <= 0 || altura >= 3.00) {
         console.log("Altura inválida!");
         alturaEhValida = false;
-        tdImc.textContent = "Altura inválida";
+        tdImc.textContent = "Altura Inválida!";
     }
 
-  if (alturaEhValida && pesoEhValido) {
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
+    if (alturaEhValida && pesoEhValido){
+        var imc = peso / (altura * altura); 
+        tdImc.textContent = imc;
     }
 }
+
+// 21/10
+// var pacientes = document.querySelectorAll(".paciente");
+
+// for (var i = 0; i < pacientes.length; i++) {
+
+//     var paciente = pacientes[i];
+
+//     var tdPeso = paciente.querySelector(".info-peso");
+//     var peso = tdPeso.textContent;
+
+//     var tdAltura = paciente.querySelector(".info-altura");
+//     var altura = tdAltura.textContent;
+
+//     var tdImc = paciente.querySelector(".info-imc");
+
+//     var pesoEhValido = true;
+//     var alturaEhValida = true;
+
+//     if (peso <= 0 || peso >= 1000) {
+//         console.log("Peso inválido!");
+//         pesoEhValido = false;
+//         tdImc.textContent = "Peso inválido";
+//     }
+
+//     if (altura <= 0 || altura >= 3.00) {
+//         console.log("Altura inválida!");
+//         alturaEhValida = false;
+//         tdImc.textContent = "Altura inválida";
+//     }
+
+//   if (alturaEhValida && pesoEhValido) {
+//         var imc = peso / (altura * altura);
+//         tdImc.textContent = imc.toFixed(2);
+//     }
+// }
 
 
 
