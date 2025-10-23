d0f4ae042f27a19ad1fda0c8e962a246f8f4a5fd
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2025-10-23 10:16:59 -0300

    1

--- js/principal.js
+++ js/principal.js
@@ -56,7 +56,7 @@ titulo.addEventListener("click", mostraMensagem);
 
 // funcao anonima = a de cima mas diferente
 titulo.addEventListener("click", function(){
-    alert("Ola eu fui clicado numa funcao anonima")
+    alert("Ola eu fui clicado numa funcao anonima");
 }
 
 // EU QUERO CHAMAR UM EVENTO DE CLICK QUANDO CLICAR NO BOTAO ADICIONAR
