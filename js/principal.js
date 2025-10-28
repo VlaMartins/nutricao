1ea8b2225cf73b1f42094b4107317aa8b043b205
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2025-10-28 09:47:58 -0300

    3

--- js/principal.js
+++ js/principal.js
@@ -58,45 +58,40 @@ titulo.addEventListener("click", function(){
 });
 
 
-
-
-
-
-
 // EU QUERO CHAMAR UM EVENTO DE CLICK QUANDO CLICAR NO BOTAO ADICIONAR
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
-    // alert("Oi eu sou o botao e fui clicado");
-    var form = document.querySelector("#form-adiciona");
+    alert("Oi eu sou o botao e fui clicado");
+   // var form = document.querySelector("#form-adiciona");
 
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+//     var nome = form.nome.value;
+//     var peso = form.peso.value;
+//     var altura = form.altura.value;
+//     var gordura = form.gordura.value;
 
-    var pacienteTr = document.createElement("tr");
+//     var pacienteTr = document.createElement("tr");
 
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
+//     var nomeTd = document.createElement("td");
+//     var pesoTd = document.createElement("td");
+//     var alturaTd = document.createElement("td");
+//     var gorduraTd = document.createElement("td");
+//     var imcTd = document.createElement("td");
 
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
+//     nomeTd.textContent = nome;
+//     pesoTd.textContent = peso;
+//     alturaTd.textContent = altura;
+//     gorduraTd.textContent = gordura;
 
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
+//     pacienteTr.appendChild(nomeTd);
+//     pacienteTr.appendChild(pesoTd);
+//     pacienteTr.appendChild(alturaTd);
+//     pacienteTr.appendChild(gorduraTd);
 
-    var tabela = document.querySelector("#tabela-pacientes");
+//     var tabela = document.querySelector("#tabela-pacientes");
 
-    tabela.appendChild(pacienteTr);
-});
+//     tabela.appendChild(pacienteTr);
+// });
 
 // ele está funcionando mas a pagina recarrega rapido pq e o comportamento esperado do botao e limpa automaticamente o console, entao por isso a gnt nao consegue ver no console.  Está certo, mas precisamos mudar o comportamento do botaão 
 
