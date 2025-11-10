e1d0bab75dd1343a0ed574490869f59aa4e9e4b8
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2025-11-10 14:40:24 -0300

    1

--- js/form.js
+++ js/form.js
@@ -5,11 +5,27 @@ botaoAdicionar.addEventListener("click", function(event) {
     var form = document.querySelector("#form-adiciona");
     var paciente = obtemPacienteDoFormulario(form);
 
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+    //cria a tr e a td do paciente
+    var pacienteTr = montaTr(paciente);
 
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+});
+
+function obtemPacienteDoFormulario(form) {
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+    return paciente;
+}
+
+// criar uma funcao que monte a tr do paciente 
+function montaTr(paciente) {
     var pacienteTr = document.createElement("tr");
 
     var nomeTd = document.createElement("td");
@@ -22,26 +38,13 @@ botaoAdicionar.addEventListener("click", function(event) {
     pesoTd.textContent = peso;
     alturaTd.textContent = altura;
     gorduraTd.textContent = gordura;
+    imcTd.textContent = paciente.imc;/*xxxxxxx */
 
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);/*xxxxxxx */
 
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-
-});
-
-function obtemPacienteDoFormulario(form) {
-
-    var paciente = {
-        nome: form.nome.value,
-        peso: form.peso.value,
-        altura: form.altura.value,
-        gordura: form.gordura.value,
-        imc: calculaImc(form.peso.value, form.altura.value)
-    }
-    return paciente;
+     return pacienteTr;  /*add */
 }
